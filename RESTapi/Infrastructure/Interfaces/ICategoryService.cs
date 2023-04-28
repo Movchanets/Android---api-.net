c15aec52949f6c6e895b7a90a8364e7fd5c7aaa3
Movchanets <slavikmovchanetc@gmail.com>
2023-04-28 20:56:29 +0300

    Validation

--- RESTapi/Infrastructure/Interfaces/ICategoryService.cs
+++ RESTapi/Infrastructure/Interfaces/ICategoryService.cs
@@ -5,5 +5,7 @@ namespace Infrastructure.Interfaces;
 public interface ICategoryService 
 {
     Task Create(CreateCategoryVm model);
+    Task Delete(int id);
     Task<List<CategoryItemVm>> GetAllAsync();
+    Task Update(UpdateCategoryVm model);
 }
